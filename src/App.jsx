6f1cb0911f14cafaa978be85baf7f9169bdcf8ef
GlowingPiranha <ikimus01@gmail.com>
2025-09-04 00:53:17 +0200

    [upd] pt-1

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,16 @@ import './App.css'
 
 function App() {
 
+  const [formData, setFormData] = useState({
+    author: "",
+    title: "",
+    body: "",
+    public: false,
+  })
+
+  const [alert, setAlert] = useState(null)
+
+
 
   return (
     <div className="container-fluid px-1 py-5 mx-auto">
@@ -12,39 +22,47 @@ function App() {
 
           <div className="col-7 col-md-9 text-center">
             <h3>Form Request</h3>
+
+            {alert && (
+              <div className={'alert alert-${alert.type}'} role="alert">
+                {alert.message}
+              </div>
+            )}
+
             <div className="card">
-
-
               <form className='p-3'>
                 <div className="mb-3">
-                  <label className="form-label">Author</label>
+                  <label className="form-label">Autore</label>
                   <input
                     type="text"
                     name="author"
                     className="form-control"
+                    value={formData.author}
                     placeholder="Inserisci l'autore"
                     required
                   />
                 </div>
 
                 <div className="mb-3">
-                  <label className="form-label">Title</label>
+                  <label className="form-label">Titolo</label>
                   <input
                     type="text"
                     name="title"
                     className="form-control"
+                    value={formData.title}
                     placeholder="Inserisci il titolo"
                     required
                   />
                 </div>
 
                 <div className="mb-3">
-                  <label className="form-label">Body</label>
+                  <label className="form-label">Content</label>
                   <textarea
                     name="body"
                     className="form-control"
                     placeholder="Scrivi il contenuto del post"
                     rows="4"
+                    value={formData.body}
                     required
                   />
                 </div>
@@ -54,12 +72,13 @@ function App() {
                     type="checkbox"
                     name="public"
                     className="form-check-input"
+                    checked={formData.public}
                   />
-                  <label className="form-check-label">Pubblico?</label>
+                  <label className="form-check-label">Public?</label>
                 </div>
 
                 <button type="submit" className="btn btn-primary w-100">
-                  Invia Post
+                  Send Post
                 </button>
               </form>
 
